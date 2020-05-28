24615d7842c5420a8dfdfca284a3f2407ee5987b
zhanglang1 <18380214028@163.com>
2020-05-28 10:07:28 +0800

    最新提交

--- js/modal/modal.js
+++ js/modal/modal.js
@@ -4,15 +4,15 @@ $(function () {
     if (!input.is(e.target) && input.has(e.target).length === 0) {
       $(".zh-select-dropdown").hide();//需要隐藏的元素
     }
-  })
+  });
 });
 // 弹窗X 按钮点击事件
-function closeModal() {
+function closeModal () {
   $('.zh-modal-box').addClass('dn');
   $('.zh-modal-background').addClass('dn');
 };
 // 动态创建(删除提示框)
-function delete_message(url, id) {
+function delete_message (url, id) {
   var zh_modal_box = $('.zh-modal-box');
   zh_modal_box.html('');
   zh_modal_box.css({
@@ -29,7 +29,7 @@ function delete_message(url, id) {
     "<div class='zh-modal-body'>" +
     "<div class='zh-message-body'>" +
     "<div class='zh-modal-icon'><img src='" + url + "' alt='图片加载失败'></div>" +
-    "<div class='zh-modal-title'>您确定要删除该科目吗？删除后不可恢复?</div>" +
+    "<div class='zh-modal-title'>您确定要删除该科目吗，删除后不可恢复！</div>" +
     "</div>" +
     "</div>" +
     "<div class='zh-modal-footer'>" +
@@ -40,14 +40,13 @@ function delete_message(url, id) {
   zh_modal_box.append(div);
 };
 //动态创建新增科目弹窗
-function create_subject(subject_name) {
+function create_subject (subject_name) {
   $('.zh-modal-box').html('');
-  debugger
   $('.zh-modal-box').css({
-    left:'50%',
-    width:'452px',
-    top:'132px',
-    height: 'auto',
+    'left': '50%',
+    'width': '452px',
+    'top': '132px',
+    'height': 'auto',
     'margin-left': '-226px',
   });
   var div = "<div class='zh-modal-content'>" +
@@ -154,4 +153,132 @@ function create_subject(subject_name) {
     "</div>" +
     "<div>";
   $('.zh-modal-box').append(div);
+};
+// 公用弹窗封装
+var modal = function (option) {
+  /**
+   * type: 弹窗类型  1： 操作型弹窗 2：提示型弹窗
+   * type_1 :操作型弹窗样式
+   * type_2 :提示型弹窗样式
+   */
+  $('.zh-modal-box').removeClass('dn');
+  $('.zh-modal-background').removeClass('dn');
+  var zh_modal_box = $('.zh-modal-box'),
+    zh_modal_content = $('.zh-modal-content');
+  zh_modal_content.html('');
+  var defaults = {
+    type: 1,
+    area: [452, 170],
+    title: "系统提示",
+  }
+  var data = $.extend({}, defaults, option);
+  zh_modal_box.css({
+    top: '50%',
+    width: data.area[0] + 'px',
+    height: data.area[1] + 'px',
+    'margin-top': - (data.area[1] / 2) + "px",
+    'margin-left': - (data.area[0] / 2) + 'px',
+  });
+  var header = "<div class='zh-modal-head'>" + data.title + "<div class='close-modal' onclick='closeModal()'>X</div></div>",
+    body = "<div class='zh-modal-body' style='max-height:" + (Number(data.area[1]) - 100) + 'px' + "'>" + modal_body() + "</div>",
+    footer = "<div class='zh-modal-footer'>" +
+      "<div class='zh-button' size='mini' onclick='closeModal()'>取消</div>" +
+      "<div class='zh-button' type='primary' size='mini' onclick='createNumber()'>确定</div>" +
+      "</div>";
+
+  zh_modal_content.append(header, body, footer);
+};
+var modal_body = function () {
+  return body = "<div class='zh-form-box'><div class='zh-form'>" +
+    "<ul class='zh-form-item'>" +
+    "<li class='zh-form-item'>" +
+    "<div class='zh-form-item_label'>科目编码</div>" +
+    "<div class='zh-form-item_content'>" +
+    "<div class='zh-input-box'>" +
+    "<input type='text' class='zh-input'>" +
+    "</div>" +
+    "</div>" +
+    "</li>" +
+    "<li class='zh-form-item'>" +
+    "<div class='zh-form-item_label'>科目名称</div>" +
+    "<div class='zh-form-item_content'>" +
+    "<div class='zh-input-box'>" +
+    "<input type='text' class='zh-input'>" +
+    "</div>" +
+    "</div>" +
+    "</li>" +
+    "<li class='zh-form-item'>" +
+    "<div class='zh-form-item_label'>上级科目</div>" +
+    "<div class='zh-form-item_content'>" +
+    "<div class='zh-input-box'>" +
+    "<input type='text' class='zh-input' disabled>" +
+    "</div>" +
+    "</div>" +
+    "</li>" +
+    "<li class='zh-form-item'>" +
+    "<div class='zh-form-item_label'>科目类别</div>" +
+    "<div class='zh-form-item_content'>" +
+    "<div class='zh-input-box' style='position:relative'>" +
+    "<input type='text' class='zh-input subjectType' readonly>" +
+    "<div class='zh-select-dropdown' style='position:absolute'>" +
+    "<ul class='zh-select-dropdown_items'>" +
+    "<li class='zh-select-dropdown_item'>流动资产</li>" +
+    "<li class='zh-select-dropdown_item'>非流动资产</li>" +
+    "</ul>" +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</li>" +
+    "<li class='zh-form-item'>" +
+    "<div class='zh-form-item_label'>余额方向</div>" +
+    "<div class='zh-form-item_content'>" +
+    "<div class='zh-input-box'>" +
+    "<input type='text' class='zh-input'>" +
+    "</div>" +
+    "</div>" +
+    "</li>" +
+    "<li class='zh-form-item'>" +
+    "<div class='zh-form-item_label'>" +
+    "<span class='zh-check'>✔</span>" +
+    "<span>辅助核算</span>" +
+    "</div>" +
+    "<div class='zh-form-item_content'>" +
+    "<div class='zh-checkbox-group'>" +
+    "<div>" +
+    "<span class='zh-check'>✔</span>" +
+    "<span>客户</span>" +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</li>" +
+    "<li class='zh-form-item'>" +
+    "<div class='zh-form-item_label'>" +
+    "<span class='zh-check'>✔</span>" +
+    "<span>数量核算</span>" +
+    "</div>" +
+    "<div class='zh-form-item_content'>" +
+    "<div class='zh-checkbox-group'>" +
+    "<div>" +
+    "<span class='zh-check'>✔</span>" +
+    "<span>客户</span>" +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</li>" +
+    "<li class='zh-form-item'>" +
+    "<div class='zh-form-item_label'>" +
+    "<span class='zh-check'>✔</span>" +
+    "<span>外币核算</span>" +
+    "</div>" +
+    "<div class='zh-form-item_content'>" +
+    "<div class='zh-checkbox-group'>" +
+    "<div>" +
+    "<span class='zh-check'>✔</span>" +
+    "<span>客户</span>" +
+    "</div>" +
+    "</div>" +
+    "</div>" +
+    "</li>" +
+    "</ul>" +
+    "</div></div>";
 }
--- js/subject/subject.js
+++ js/subject/subject.js
@@ -1,69 +1,69 @@
 
-httpUrl={
+httpUrl = {
   getMenu: '/api/subject/classification/get/allSubjectClassification',//查询头部导航栏
-  getTable:'/api/subject/findlist', //查询表格数据
+  getTable: '/api/subject/findlist', //查询表格数据
   subjectEnable: '/api/subject/enable', //状态切换
 };
- //查询表格数据传递的参数
- var formTable = {
+//查询表格数据传递的参数
+var formTable = {
   pageNum: 1,
   pageSize: 10000,
   subjectCategory: "ASSETS"
 };
-$(function(){
+$(function () {
   asyncInit();
   //科目类别输入框获得焦点
-  $('.zh-form').on('focus','.subjectType',function(){
-    $('.zh-select-dropdown').css({'display':'block'})
+  $('.zh-form').on('focus', '.subjectType', function () {
+    $('.zh-select-dropdown').css({ 'display': 'block' })
   });
   //科目类别点击选择
-  $('.zh-form .zh-select-dropdown').on('click','.zh-select-dropdown_item',function(){
+  $('.zh-form .zh-select-dropdown').on('click', '.zh-select-dropdown_item', function () {
     var target = $(this).parents('.zh-input-box').children('.subjectType');
-    var text=$(this).text();
+    var text = $(this).text();
     target.val(text);
-    $(this).parents('.zh-select-dropdown').css({'display':'none'})
+    $(this).parents('.zh-select-dropdown').css({ 'display': 'none' })
   });
 });
 //同步执行
-async function asyncInit(){
+async function asyncInit () {
   await getMenu();
   await getTableData();
 }
 //loading 效果的添加和移除
-var loading = function setLoading(data){
+var loading = function setLoading (data) {
   return data == true ? $('.zh-loading').removeClass('dn') : $('.zh-loading').addClass('dn');
 }
 //导航菜单切换
-$('.zh-menubar').on('click','li',function(){
-  if($(this).hasClass('zh-active')){return};
+$('.zh-menubar').on('click', 'li', function () {
+  if ($(this).hasClass('zh-active')) { return };
   $(this).addClass('zh-active').siblings().removeClass('zh-active');
-  var sType=$(this).children('div').attr('sType');
-  var iTexy=$(this).children('div').text();
+  var sType = $(this).children('div').attr('sType');
+  var iTexy = $(this).children('div').text();
   formTable.subjectCategory = sType;
   getTableData();
 });
 //多选框点击事件
-$('.zh-table').on('click','.zh-checkbox',function(){
+$('.zh-table').on('click', '.zh-checkbox', function () {
   $(this).toggleClass('zh-checked');
 });
 //开关按钮点击事件
-$('.zh-table').on('click','.zh-switch',function(){
-  if($(this).hasClass('selected')){
+$('.zh-table').on('click', '.zh-switch', function () {
+  if ($(this).hasClass('selected')) {
     $(this).removeClass('selected');
-  }else{
+  } else {
     $(this).addClass('selected');
   }
 });
 //查询头部导航菜单
-function getMenu(){
+function getMenu () {
   loading(true);
   return Https({
-    url:httpUrl.getMenu,
-    type:"get",
-    data:{},
-    success:function(res){
+    url: httpUrl.getMenu,
+    type: "get",
+    data: {},
+    success: function (res) {
       loading(false);
-      if(res && res.length > 0){
+      if (res && res.length > 0) {
         setMenu(res);
         formTable.subjectCategory = res[0].code;
       }
@@ -71,130 +71,138 @@ function getMenu(){
   })
 };
 //查询列表数据
-function getTableData(){
+function getTableData () {
   loading(true);
   return Https({
-    url:httpUrl.getTable,
-    type:'post',
-    data:formTable,
-    success:function(res){
+    url: httpUrl.getTable,
+    type: 'post',
+    data: formTable,
+    success: function (res) {
       loading(false);
       setTableData(res);
     },
   })
 };
 //新增按钮点击事件
-function add_subject(){
+function add_subject () {
   $('.zh-modal-box').removeClass('dn');
   $('.zh-modal-background').removeClass('dn');
   create_subject('资产');
 };
+// 编辑按钮点击事件
+function editRow () {
+  var option = {
+    type: 1,
+    area: [452, 530],
+  }
+  modal(option)
+};
 //删除按钮(单个删除)
-function deletRow(id){ //id:要删除的数据的id
+function deletRow (id) { //id:要删除的数据的id
   $('.zh-modal-background').removeClass('dn');
   $('.zh-modal-box').removeClass('dn');
-  delete_message('../../../img/confirm.gif',id); //创建删除提示框
+  delete_message('../../../img/confirm.gif', id); //创建删除提示框
 };
 //确定删除
-function delet_this(id){
+function delet_this (id) {
   console.log(id)
 };
 //设置导航栏数据
-function setMenu(res){
+function setMenu (res) {
   $('#subjectMenu').html(""); //清空原始值
-  for(var i=0;i<res.length;i++){
-    if(i == 0){
-      var  li ="<li class='zh-active'><div sType='"+res[i].code+"'>"+res[i].name+"</div></li>";
-    }else{
-      var  li ="<li><div sType='"+res[i].code+"'>"+res[i].name+"</div></li>";
+  for (var i = 0; i < res.length; i++) {
+    if (i == 0) {
+      var li = "<li class='zh-active'><div sType='" + res[i].code + "'>" + res[i].name + "</div></li>";
+    } else {
+      var li = "<li><div sType='" + res[i].code + "'>" + res[i].name + "</div></li>";
     }
-   $('#subjectMenu').append(li);
-  } 
+    $('#subjectMenu').append(li);
+  }
 };
 // 设置表格数据
-function setTableData(res){
+function setTableData (res) {
   $('.zh-table tbody').html('');
-  if(res && res.length > 0){
+  if (res && res.length > 0) {
     var tr;
-    for(var i = 0;i<res.length;i++){
-      tr+="<tr id="+res[i].id+">"+
-      "<td checkbox><div class='zh-checkbox'>✔</div></td>"+
-      "<td>"+res[i].code+"</td>";
-      if(res[i].subjectSeries == 1){
-        tr+= "<td align='left'><p>"+res[i].name+"</p></td>";
-      }else if(res[i].subjectSeries == 2){
-        tr+= "<td align='left'><p style='margin-left:10px;'>"+res[i].name+"</p></td>";
-      }else if(res[i].subjectSeries == 3){
-        tr+= "<td align='left'><p style='margin-left:20px;'>"+res[i].name+"</p></td>";
-      }else if(res[i].subjectSeries == 4){
-        tr+= "<td align='left'><p style='margin-left:30px;'>"+res[i].name+"</p></td>";
-      }else if(res[i].subjectSeries == 5){
-        tr+= "<td align='left'><p style='margin-left:40px;'>"+res[i].name+"</p></td>";
-      }else{
-        tr+= "<td align='left'><p style='margin-left:50px;'>"+res[i].name+"</p></td>";
+    for (var i = 0; i < res.length; i++) {
+      tr += "<tr id=" + res[i].id + ">" +
+        "<td checkbox><div class='zh-checkbox'>✔</div></td>" +
+        "<td>" + res[i].code + "</td>";
+      if (res[i].subjectSeries == 1) {
+        tr += "<td align='left'><p>" + res[i].name + "</p></td>";
+      } else if (res[i].subjectSeries == 2) {
+        tr += "<td align='left'><p style='margin-left:10px;'>" + res[i].name + "</p></td>";
+      } else if (res[i].subjectSeries == 3) {
+        tr += "<td align='left'><p style='margin-left:20px;'>" + res[i].name + "</p></td>";
+      } else if (res[i].subjectSeries == 4) {
+        tr += "<td align='left'><p style='margin-left:30px;'>" + res[i].name + "</p></td>";
+      } else if (res[i].subjectSeries == 5) {
+        tr += "<td align='left'><p style='margin-left:40px;'>" + res[i].name + "</p></td>";
+      } else {
+        tr += "<td align='left'><p style='margin-left:50px;'>" + res[i].name + "</p></td>";
       }
-      tr+="<td align='left'>"+res[i].subjectDetailCategoryValue+"</td>";
+      tr += "<td align='left'>" + res[i].subjectDetailCategoryValue + "</td>";
       //判断余额方向
-      if(res[i].balanceDirection == 'DEBIT'){
-        tr+="<td>借</td>"
-      }else{
-        tr+="<td>贷</td>"
+      if (res[i].balanceDirection == 'DEBIT') {
+        tr += "<td>借</td>"
+      } else {
+        tr += "<td>贷</td>"
       };
       //渲染辅助核算
-      if(res[i].auxiliaryCategoryList){
-        if(res[i].auxiliaryCategoryList.length>0){
-          tr+="<td align='left'>";
-          for(var j=0;j<res[i].auxiliaryCategoryList.length;j++){
-            tr+="<span>"+res[i].auxiliaryCategoryList[j].name+"</span>"
+      if (res[i].auxiliaryCategoryList) {
+        if (res[i].auxiliaryCategoryList.length > 0) {
+          tr += "<td align='left'>";
+          for (var j = 0; j < res[i].auxiliaryCategoryList.length; j++) {
+            tr += "<span>" + res[i].auxiliaryCategoryList[j].name + "</span>"
           }
-          tr+= "</td>"
-        }else{
-          tr+="<td></td>";
+          tr += "</td>"
+        } else {
+          tr += "<td></td>";
         }
       };
       //渲染外币
-      if(res[i].currencyList){
-        if(res[i].currencyList.length>0){
-          tr+="<td align='left'>";
-          for(var j=0;j<res[i].currencyList.length;j++){
-            tr+="<span>"+res[i].currencyList[j].code+"</span>";
+      if (res[i].currencyList) {
+        if (res[i].currencyList.length > 0) {
+          tr += "<td align='left'>";
+          for (var j = 0; j < res[i].currencyList.length; j++) {
+            tr += "<span>" + res[i].currencyList[j].code + "</span>";
           }
-          tr+= "</td>";
-        }else{
-          tr+="<td></td>";
+          tr += "</td>";
+        } else {
+          tr += "<td></td>";
         }
       }
-      tr+="<td>"+res[i].unit+"</td>";
-      if(res[i].whetherEnable == 1){
-        tr+="<td><div class='zh-switch selected' whetherEnable="+res[i].whetherEnable+"></div></td>"
-      }else{
-        tr+="<td><div class='zh-switch' whetherEnable="+res[i].whetherEnable+"></div></td>"
+      tr += "<td>" + res[i].unit + "</td>";
+      if (res[i].whetherEnable == 1) {
+        tr += "<td><div class='zh-switch selected' whetherEnable=" + res[i].whetherEnable + "></div></td>"
+      } else {
+        tr += "<td><div class='zh-switch' whetherEnable=" + res[i].whetherEnable + "></div></td>"
       }
-      tr+="<td style='width:235px;'>"+
-        "<div class='zh-button' size='small' type='primary'>增加下级</div>"+
-        "<div class='zh-button' size='small'>修改</div>"+
-        "<div class='zh-button' size='small' onclick='deletRow("+res[i].id+")'>删除</div>"+
-      "</td></tr>";
+      tr += "<td style='width:235px;'>" +
+        "<div class='zh-button' size='small' type='primary'>增加下级</div>" +
+        "<div class='zh-button' size='small' onclick = 'editRow(" + res[i].id + ")'>修改</div>" +
+        "<div class='zh-button' size='small' onclick='deletRow(" + res[i].id + ")'>删除</div>" +
+        "</td></tr>";
     };
     $('.zh-table tbody').append(tr);
   }
 }
 //动态创建弹窗的内容
-function createModal(){
+function createModal () {
 
 }
 let number = [];
 //确定按钮点击事件
-function createNumber(){
-  let q_number = Math.floor(30*Math.random()+5);
-  let h_number = Math.floor(10*Math.random()+2);
-  if(number.length < 7){
-    if(number.length <6 ){
-      if(number.indexOf(q_number) == -1){
+function createNumber () {
+  let q_number = Math.floor(30 * Math.random() + 5);
+  let h_number = Math.floor(10 * Math.random() + 2);
+  if (number.length < 7) {
+    if (number.length < 6) {
+      if (number.indexOf(q_number) == -1) {
         number.push(q_number);
       }
-    }else{
-      if(number.indexOf(h_number) == -1){
+    } else {
+      if (number.indexOf(h_number) == -1) {
         number.push(h_number.toString());
       }
     }
